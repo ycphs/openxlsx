60815eb73f13e36c44de8db974223a8761e950f2
Philipp Schauberger <philipp@schauberger.co.at>
2019-11-27 18:29:02 +0100

    azure test

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,2 +1,8 @@
-library("testthat")
-test_check("openxlsx")
+library(testthat)
+library(openxlsx)
+
+if (requireNamespace("xml2")) {
+  test_check("openxlsx", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
+} else {
+  test_check("openxlsx")
+}
