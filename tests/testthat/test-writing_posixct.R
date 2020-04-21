d707dbcb6f7ce17fbe675cad11621c68f76b2a82
Blake Boswell <j.blake.boswell@gmail.com>
2020-04-20 22:07:30 -0400

    modified testthat for write mixed est/edt
    the previous test that sets `expected` to a static vector collected from
    excel.  this version sets `expected` value equal to the data that is
    written.

--- tests/testthat/test-writing_posixct.R
+++ tests/testthat/test-writing_posixct.R
@@ -41,14 +41,15 @@ test_that("Writing Posixct with writeData & writeDataTable", {
   
 })
 
+
 test_that("Writing mixed EDT/EST Posixct with writeData & writeDataTable", {
   
   options("openxlsx.datetimeFormat" = "dd/mm/yy hh:mm")
   
   tstart1 <- strptime("12/03/2018 08:30", "%d/%m/%Y %H:%M", tz="America/New_York")
   tstart2 <- strptime("10/03/2018 08:30", "%d/%m/%Y %H:%M", tz="America/New_York")
-  TimeDT1 <- c(0,10,30,60,120,240,720,1440)*60 + tstart1
-  TimeDT2 <- c(0,10,30,60,120,240,720,1440)*60 + tstart2
+  TimeDT1 <- c(NA, 0,10,30,60,120,240,720,1440)*60 + tstart1
+  TimeDT2 <- c(0,10,30,60,120,240,720,NA,1440)*60 + tstart2
   
   df <- data.frame(timeval = c(TimeDT1, TimeDT2),
                    timetxt = format(c(TimeDT1, TimeDT2),"%Y-%m-%d %H:%M"))
@@ -60,16 +61,23 @@ test_that("Writing mixed EDT/EST Posixct with writeData & writeDataTable", {
   writeData(wb, "writeData", df, startCol = 2, startRow = 3, rowNames = FALSE)
   writeDataTable(wb, "writeDataTable", df, startCol = 2, startRow = 3)
   
-  wd <- as.numeric(wb$worksheets[[1]]$sheet_data$v)
+  wd  <- as.numeric(wb$worksheets[[1]]$sheet_data$v)
   wdt <- as.numeric(wb$worksheets[[2]]$sheet_data$v)
+  wd  <- wd[wb$worksheets[[1]]$sheet_data$cols == 2]
+  wdt <- wdt[wb$worksheets[[2]]$sheet_data$cols == 2]
   
-  expected <- c(0, 1, 43171.354166666700621, 2, 43171.361111111102218, 3, 43171.375, 4, 43171.395833333299379,
-                5, 43171.4375, 6, 43171.520833333299379, 7, 43171.854166666700621, 8, 43172.354166666700621,
-                9, 43169.354166666700621, 10, 43169.361111111102218, 11, 43169.375, 12, 43169.395833333299379,
-                13, 43169.4375, 14, 43169.520833333299379, 15, 43169.854166666700621, 16, 43170.395833333299379, 17)
+  # drop any integer indexes introduced in write
+  wd  <- wd[wd%%1 > 0 | is.na(wd)]
+  wdt <- wdt[wdt%%1 > 0 | is.na(wdt)]
   
-  expect_equal(object = round(wd, 12), expected = round(expected, 12))
-  expect_equal(object = round(wdt, 12), expected = round(expected, 12))
+  # sort everything
+  wd  <- convertToDateTime(wd[order(wd)])
+  wdt <- convertToDateTime(wdt[order(wdt)])
+  expected <- df$timeval[order(df$timeval)]
+  
+  # compare
+  expect_equal(object = wd, expected = expected, tolerance = 10^-10)
+  expect_equal(object = wdt, expected = expected, tolerance = 10^-10)
   expect_equal(object = wd, expected = wdt)
   
   options("openxlsx.datetimeFormat" = "yyyy-mm-dd hh:mm:ss")
