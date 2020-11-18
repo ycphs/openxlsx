333abb726761c51768cace6cc93767db367faa76
shrektan <shrektan@126.com>
2020-11-18 18:45:35 +0800

    add a clone argument

--- src/helper_functions.cpp
+++ src/helper_functions.cpp
@@ -315,10 +315,16 @@ std::vector<std::string> get_letters(){
 
 
 // [[Rcpp::export]]
-CharacterVector markUTF8(CharacterVector x) {
+CharacterVector markUTF8(CharacterVector x, bool clone) {
+  CharacterVector out;
+  if (clone) {
+    out = Rcpp::clone(x);
+  } else {
+    out = x;
+  }
   const size_t n = x.size();
   for (size_t i = 0; i < n; ++i) {
-    x[i] = Rf_mkCharCE(x[i], CE_UTF8);
+    out[i] = Rf_mkCharCE(x[i], CE_UTF8);
   }
-  return x;
+  return out;
 }
