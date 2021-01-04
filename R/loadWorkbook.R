e7eba47bff351fa66e7d2832a10bfc9b26ae5387
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2021-01-04 03:33:52 +0100

    fix comment Id

--- R/loadWorkbook.R
+++ R/loadWorkbook.R
@@ -831,24 +831,16 @@ loadWorkbook <- function(file, xlsxFile = NULL, isUnzipped = FALSE) {
     ## vmlDrawing and comments
     if (length(commentsXML) > 0) {
 
-      com_rId <- vector("list", length(commentsrelXML))
-      names(com_rId) <- commentsrelXML
-      for (com_rel in commentsrelXML) {
-        # TODO: I do not get this, why does this work with the print and not without it?
-        rel_xml <- readXMLPtr(com_rel)
-        attrs <- getXMLXPtr2attr(rel_xml, "Relationships", "Relationship")
-        #print(attrs)
-        #rels <- getXMLXPtr2(rel_xml, "Relationships", "Relationship")
-        #print(rels)
-        #attrs <- getXMLattr(rels, "Relationship")
-        #print(attrs)
-        # commentID <- sapply(attrs, FUN= function(x)any(grepl(x, pattern = "comments")))
-        # s <- unlist(attrs[commentID])
-        attrs <- as.data.frame(do.call("rbind", attrs))
-        # print(attrs)
-        com_rId[[com_rel]] <- attrs
-      }
-      com_rId <<- com_rId
+      # com_rId <- NULL
+      # for (com_rel in commentsrelXML) {
+      #   rel_xml <<- readXML(com_rel)
+      #   rels <<- getXML2(rel_xml, "Relationships", "Relationship")
+      #   attrs <<- getXMLattr(rels, "Relationship")
+      #   commentID <- sapply(attrs, FUN= function(x)any(grepl(x, pattern = "comments")))
+      #   s <- unlist(attrs[commentID])
+      #   com_rId <- c(com_rId, as.character(s[names(s)=="Id"]))
+      # }
+      # com_rId <<- com_rId
       drawXMLrelationship <- lapply(xml, function(x) x[grepl("drawings/vmlDrawing[0-9]+\\.vml", x)])
       hasDrawing <- sapply(drawXMLrelationship, length) > 0 ## which sheets have a drawing
 
