7e6a2b484e0fed3968f0a062b25c34a2b67ad0e1
Jeff Sullivan <jeff.sullivan@precisionvh.com>
2021-06-24 23:33:12 -0400

    test to fail when write permissions denied doesn't cause error

--- tests/testthat/test-saveWorkbook.R
+++ tests/testthat/test-saveWorkbook.R
@@ -3,17 +3,26 @@ context("save workbook")
 
 
 test_that("test return values for saveWorkbook", {
-  
   tempFile <- temp_xlsx()
-  wb<-createWorkbook()
-  addWorksheet(wb,"name")
-  expect_true( saveWorkbook(wb,tempFile,returnValue = TRUE))
-  
-  expect_error( saveWorkbook(wb,tempFile,returnValue = TRUE))
-  
-  
-  expect_invisible( saveWorkbook(wb,tempFile,returnValue = FALSE ,overwrite = TRUE))
+  wb <- createWorkbook()
+  addWorksheet(wb, "name")
+  expect_true(saveWorkbook(wb, tempFile, returnValue = TRUE))
+
+  expect_error(saveWorkbook(wb, tempFile, returnValue = TRUE))
+
+
+  expect_invisible(saveWorkbook(wb, tempFile, returnValue = FALSE, overwrite = TRUE))
   unlink(tempFile, recursive = TRUE, force = TRUE)
-  
-}
-)
+})
+
+test_that("test failed write errors for saveWorkbook", {
+  tempFile <- fs::file_temp(ext = "xlsx")
+  fs::file_create(tempFile, mode = "a=rx")
+
+  wb <- createWorkbook()
+  addWorksheet(wb, "name")
+
+  expect_error(saveWorkbook(wb, tempFile, returnValue = TRUE, overwrite = TRUE))
+
+  unlink(tempFile, recursive = TRUE, force = TRUE)
+})
