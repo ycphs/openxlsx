9d41f7b2b37448fcf599de1ee038903d8abb3b9e
jmbarbone <jmbarbone@gmail.com>
2021-07-02 15:00:37 -0400

    resolves #215
    * adds correct return() for the list functions
    * invisible() used to match the outputs for non-list
    * cleans up the test-options.R

--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -5,17 +5,27 @@ test_that("option names are appropriate", {
 
 test_that("changing options", {
   op <- options()
-  on.exit(options(op), add = TRUE)
   
   # Set via options()
-  options(openxlsx.border = "whatever")
+  options(openxlsx.borders = "whatever")
   expect_equal(openxlsx_getOp("borders"), getOption("openxlsx.borders"))
+  expect_equal(openxlsx_getOp("borders"), "whatever")
+  
+  # Set via openxlsx_setOp()
+  openxlsx_setOp("borders", "new_whatever")
+  expect_equal(openxlsx_getOp("borders"), getOption("openxlsx.borders"))
+  expect_equal(openxlsx_getOp("borders"), "new_whatever")
+  
+  # with openxlsx. prefix
+  openxlsx_setOp("openxlsx.borders", "new_new_whatever")
+  expect_equal(openxlsx_getOp("openxlsx.borders"), getOption("openxlsx.borders"))
+  expect_equal(openxlsx_getOp("openxlsx.borders"), "new_new_whatever")
   
   options(openxlsx.tableStyle = "Cool format")
   expect_equal(openxlsx_getOp("tableStyle"), openxlsx_getOp("openxlsx.tableStyle"))
   
   # Setting to NULL will return default
-  options(openxlsx.border = NULL)
+  options(openxlsx.borders = NULL)
   expect_equal(openxlsx_getOp("borders"), op.openxlsx[["openxlsx.borders"]])
   
   # Bad options names will trigger warning but still be produced
@@ -27,4 +37,12 @@ test_that("changing options", {
   
   # Multiple Ops returns error
   expect_error(openxlsx_getOp(c("withFilter", "borders")), "length 1")
+  
+  options(op)
+})
+
+test_that("openxlsx_setOp() works with list [#215]", {
+  op <- options()
+  expect_error(openxlsx_setOp(list(withFilter = TRUE, keepNA = TRUE)), NA)
+  options(op)
 })
