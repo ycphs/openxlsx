99273e78664644f92e2eb909da9c37f3ee7cea5a
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2021-10-20 19:17:34 +0200

    guard against illegal characters in xml string

--- R/baseXML.R
+++ R/baseXML.R
@@ -72,8 +72,8 @@ genClientData <- function(col, row, visible, height, width) {
 genBaseCore <- function(creator = "", title = NULL, subject = NULL, category = NULL) {
   core <- '<coreProperties xmlns="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">'
 
-  core <- stri_c(core, sprintf("<dc:creator>%s</dc:creator>", creator))
-  core <- stri_c(core, sprintf("<cp:lastModifiedBy>%s</cp:lastModifiedBy>", creator))
+  core <- stri_c(core, sprintf("<dc:creator>%s</dc:creator>", replaceIllegalCharacters(creator)))
+  core <- stri_c(core, sprintf("<cp:lastModifiedBy>%s</cp:lastModifiedBy>", replaceIllegalCharacters(creator)))
   core <- stri_c(core, sprintf('<dcterms:created xsi:type="dcterms:W3CDTF">%s</dcterms:created>', format(Sys.time(), "%Y-%m-%dT%H:%M:%SZ")))
 
   if (!is.null(title)) {
