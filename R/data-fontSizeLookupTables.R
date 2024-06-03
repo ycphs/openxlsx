0d3e9f6894a61b7fb6d4e2579927801bd84f8d2a
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2024-06-03 21:20:01 +0200

    [roxygen] updates

--- R/data-fontSizeLookupTables.R
+++ R/data-fontSizeLookupTables.R
@@ -1,8 +1,11 @@
 #' Font Size Lookup tables
-#' 
-#' Lookup tables for font size 
-#' 
+#'
+#' Lookup tables for font size
+#'
 #' @format A data.frame with column names corresponding to font names
+#' @examples
+#' data(openxlsxFontSizeLookupTable)
+#' data(openxlsxFontSizeLookupTableBold)
 "openxlsxFontSizeLookupTable"
 
 #' @rdname openxlsxFontSizeLookupTable
